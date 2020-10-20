1810f1e075b71d3aac605d91269ff5c6fd0fd15a
rallen <rallen@gmail.com>
2020-10-19 22:32:56 -0700

    adjust version to 1.1 in about box
    match appveyor.yml

---
+++ resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WinGridStrument.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINGRIDSTRUMENT_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINGRIDSTRUMENT             107
+#define IDI_SMALL                       108
+#define IDC_WINGRIDSTRUMENT             109
+#define IDR_MAINFRAME                   128
+#define IDD_PREFS_DIALOG                129
+#define IDC_GUITAR_MODE                 1000
+#define IDC_PITCH_BEND_RANGE            1001
+#define IDC_MODULATION_CONTROLLER       1002
+#define IDC_MIDI_DEV_COMBO              1003
+#define IDC_CHANNEL_PER_ROW_MODE        1004
+#define IDC_MIDI_CHANNEL_MIN            1005
+#define IDC_MIDI_CHANNEL_MAX            1006
+#define IDC_GRID_SIZE                   1007
+#define IDC_PITCH_BEND_RANGE2           1008
+#define IDC_PITCH_BEND_MASK             1008
+#define IDC_COLOR_THEME_COMBO           1009
+#define IDC_HEX_GRID_MODE               1010
+#define IDC_PLAY_MIDI                   1011
+#define IDC_SOUNDFONT_PATH              1012
+#define IDC_PLAY_SOUNDFONT              1013
+#define ID_FILE_PREFERENCES             32771
+#define IDM_PREFS                       32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
